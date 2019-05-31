848097d0805948fd9c5d4431e5fda3bb0e4c5583
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-05-31 15:18:39 +0200

    Ajout de profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,7 +1,7 @@
 package dev;
 
 import java.io.IOException;
-
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -9,15 +9,21 @@ import com.github.lalyos.jfiglet.FigletFont;
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+    	
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	
         String asciiArt="";
 		try {
-			asciiArt = FigletFont.convertOneLine("App");
+			asciiArt = FigletFont.convertOneLine(titre);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
         System.out.println(asciiArt);
+        
+        // affichage de l'environnement
+        String environment = ResourceBundle.getBundle("application").getString("environment");
+        System.out.println("Environment : " + environment);
     }
 }
